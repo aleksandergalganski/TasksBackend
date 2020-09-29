ca2c5597a52907662bfccfad0d39cbadde777737
olekgalgan <olekgalgan@gmail.com>
2020-09-29 17:03:11 +0200

    Add validation using express-validator

---
+++ src/middleware/validator.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
+
+export const taskValidationRules = () => [
+  check('name', 'Please add a name').notEmpty(),
+  check('categoryId', 'Please add category Id').notEmpty()
+];
+
+export const userValidationRules = () => [
+  check('email', 'Email is invalid').isEmail(),
+  check('password', 'Password must be at least 8 characters').isLength({
+    min: 8
+  })
+];
+
+export const categoryValidationRules = () => [
+  check('name', 'Please add a name').notEmpty()
+];
+
+const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  const errorMessages = errors
+    .array()
+    .map(err => err.msg)
+    .join(', ');
+  return res.status(400).json({ message: errorMessages });
+};
+
+export default validate;
