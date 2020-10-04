4f93d475457d8fba8a55ad43fc5992bcc76345de
olekgalgan <olekgalgan@gmail.com>
2020-10-04 19:28:50 +0200

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ module.exports = {
   url: process.env.DB_URL,
   synchronize: true,
   logging: false,
-  entities: ['dist/src/entity/**/*.js'],
+  entities: [__dirname + '/dist/src/entity/**/*.js'],
   migrations: ['dist/src/migration/**/*.js'],
   subscribers: ['dist/src/subscriber/**/*.js'],
   cli: {
